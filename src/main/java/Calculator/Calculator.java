220d8dce1a37204a0d26260d7f18706cc48cfe95
Abhisek1906 <abhisekmishra150@gmail.com>
2023-03-03 11:18:03 +0530

    Added the mul function

--- src/main/java/Calculator/Calculator.java
+++ src/main/java/Calculator/Calculator.java
@@ -18,9 +18,9 @@ public class Calculator {
         return a-b;
     }
 
-    public long mul(long a ,long b){
-        return a*b;
-    }
+//    public long mul(long a ,long b){
+//        return a*b;
+//    }
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         Calculator calculator=new Calculator();
@@ -28,6 +28,6 @@ public class Calculator {
         long b=sc.nextLong();
         System.out.println(calculator.add(a,b));
         System.out.println(calculator.sub(a,b));
-        System.out.println(calculator.mul(a,b));
+        //System.out.println(calculator.mul(a,b));
     }
 }
